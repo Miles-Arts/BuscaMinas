717a00c8ff2ae0a655cd2027707a313a77041448
Milton Figueredo Miles <gratno@gmail.com>
2024-03-16 20:50:08 -0500

    add entity

---
+++ src/Casilla.java
@@ -0,0 +1,30 @@
+public class Casilla {
+
+    int posfila;
+    int posColumna;
+    boolean mina;
+
+    public int getPosfila() {
+        return posfila;
+    }
+
+    public void setPosfila(int posfila) {
+        this.posfila = posfila;
+    }
+
+    public int getPosColumna() {
+        return posColumna;
+    }
+
+    public void setPosColumna(int posColumna) {
+        this.posColumna = posColumna;
+    }
+
+    public boolean isMina() {
+        return mina;
+    }
+
+    public void setMina(boolean mina) {
+        this.mina = mina;
+    }
+}
